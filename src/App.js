172efd53cec3f548bb637fb87a1e0804d1cddf02
Sayeem Abdullah <sayeem.mdabdullah@gmail.com>
2021-02-14 02:25:40 +0600

    Added Login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Login from "./components/login";
+import { render } from '@testing-library/react';
 
-function App() {
+
+// const Body = props => (
+//   <p>{props.text}</p>
+// )
+
+// class Header extends Component{
+//   render(){
+    
+//     return(
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//         {this.props.title} <br></br>
+//         {this.props.num} <br></br>
+//         {this.props.myfunc(5,5)}
+//         </p>
+//         </header>
+//     );
+//   }
+// }
+
+
+class App extends Component{
+  render(){
+    // const add = (a,b) => a + b;
   return (
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+      <Login/>
+      /* <Header 
+      title="Hello"
+      num={5} 
+      myfunc={add}
+      />
+      <Body text="This is the body"/> */}
     </div>
   );
+}
 }
 
 export default App;
