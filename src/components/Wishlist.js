2cde03f41b994ec724280a2adfd353839c8adba4
Sayeem Abdullah <sayeem.mdabdullah@gmail.com>
2021-02-14 21:55:23 +0600

    Added Components

---
+++ src/components/Wishlist.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class wishlist extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default wishlist;
