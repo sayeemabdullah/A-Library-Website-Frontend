a840e329e8d6cdff4bc8fe0918c5e246cc853602
Sayeem Abdullah <sayeem.mdabdullah@gmail.com>
2021-02-14 21:52:21 +0600

    Deleted Components

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
-import Login from "./components/login";
 import { render } from '@testing-library/react';
 
-
-// const Body = props => (
-//   <p>{props.text}</p>
-// )
-
-// class Header extends Component{
-//   render(){
-    
-//     return(
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//         {this.props.title} <br></br>
-//         {this.props.num} <br></br>
-//         {this.props.myfunc(5,5)}
-//         </p>
-//         </header>
-//     );
-//   }
-// }
 
 
 class App extends Component{
   render(){
-    // const add = (a,b) => a + b;
+
   return (
   
     <div className="App">
       {
-      <Login/>
-      /* <Header 
-      title="Hello"
-      num={5} 
-      myfunc={add}
-      />
-      <Body text="This is the body"/> */}
+      
+      }
     </div>
   );
 }
